cad69f577333370edc04e38429649531877aeeb7
Shariq Shaikh <shariqshaikh2705@gmail.com>
2025-04-18 09:57:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+
+let promise1 = new Promise((resolve,reject)=>{
+	setTimeout(()=>{
+		resolve("Hello, world!");
+	},1000);
+});
+
+promise1.then((e)=>{
+	let output = document.getElementById("output");
+	output.innerText = e;
+})
